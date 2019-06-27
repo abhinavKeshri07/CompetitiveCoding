5193d5f0f77727285503041df92e39a388f6a517
abhinavKeshri07 <keshri.abhinav07@gmail.com>
2019-06-28 00:13:23 +0530

    added basics of BST in BST.java

---
+++ BinarySearchTree/BST.java
@@ -0,0 +1,86 @@
+class BST {
+    static class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+    // root node
+    static Node root = null;
+
+    static Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = insertRec(root.right, key);
+        }
+        return root;
+    }
+    static void inorderRec(Node root){
+        if(root != null)
+        {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
+    }
+    static void inorder(){
+        inorderRec(root);
+    }
+
+    static void insert(int key) {
+        root = insertRec(root, key);
+    }
+    public static  Node search(Node root, int key){
+        if(root == null || root.key == key) return root;
+        else if(root.key > key) return search(root.left, key);
+        else return search(root.right, key);
+    }
+    static void deleteKey(int key){root = deleteRec(root, key);}
+    static Node deleteRec(Node root, int key){
+        if(root == null) return root;// if the tree is empty.
+        else if(key > root.key) root.right = deleteRec(root.right, key); // otherwise
+        else if(key < root.key)root.left = deleteRec(root.left, key);
+        else{// node with only one child or no child
+            if(root.left == null)
+                return root.right;
+            else if(root.right == null)
+                return root.left;
+
+            // node with two children : get the inorder successor in right subtree.
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+        return root;
+    }
+    static int minValue(Node root){
+        int minv = root.key;
+        while(root.left != null)
+        {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    public static void main(String args[]){
+        insert(50);
+        insert(20);
+        insert(30);
+        insert(500);
+        insert(45);
+        inorder();
+        Node n = search(root, 501);
+        if(n != null)
+        {
+            System.out.println(n.key);
+        }
+    }
+}
